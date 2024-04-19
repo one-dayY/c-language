d07997568ab4901efb1596af78c6b3d7997b2914
One-dayY <yyyyyeah111@gmail.com>
2024-04-19 22:23:09 +0800

    用筛选法求n以内的素数

---
+++ prime_number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int n = 0;
+	int i = 0;
+	scanf("%d\n",&n);
+	
+	for(i = 2;i <= n; i++)//0和1都不时素数，i直接从2开始 
+	{
+		int flag = 1;//flag是1，表示是素数
+		int j = 0;
+		
+		for(j = 2;j <= sqrt(i) ; j++)
+		{
+			if(i % j == 0)
+			{
+				flag = 0;
+				break;
+			}
+		}
+		
+		if(flag == 1)//如果i不是素数，则不对flag进行改变 
+		{
+			printf("%d\n",i);
+		} 
+	}
+	
+	return 0;
+} 
